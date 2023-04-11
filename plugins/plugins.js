0a4ac33b45ac274f2832f7fd6f6d63b4312b030c
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-04-11 23:08:16 +0100

    more on gsap

--- plugins/plugins.js
+++ plugins/plugins.js
@@ -5,20 +5,20 @@
 
 
 //register the plugin (just once)
-// gsap.registerPlugin(MotionPathPlugin);
+gsap.registerPlugin(MotionPathPlugin);
 
-// gsap.set(".astronaut", { scale: 0.3, autoAlpha: 1 });
+gsap.set(".astronaut", { scale: 0.3, autoAlpha: 1 });
 
-// gsap.to(".astronaut", {
-//     duration: 5,
-//     ease: "power1.inOut",
-//     immediateRender: true,
-//     motionPath: {
-//         path: "#path",
-//         align: "#path",
-//         alignOrigin: [0.5, 0.5],
-//         autoRotate: 90
-//     }
-// });
+gsap.to(".astronaut", {
+    duration: 5,
+    ease: "power1.inOut",
+    immediateRender: true,
+    motionPath: {
+        path: "#path",
+        align: "#path",
+        alignOrigin: [0.5, 0.5],
+        autoRotate: 90
+    }
+});
 
 // MotionPathHelper.create(".astronaut");
